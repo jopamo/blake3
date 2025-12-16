3a2473e2187ec4bd8bae38acaf96060eb71a2b93
Paul Moses <p@1g4.org>
2025-12-16 09:42:54 -0600

    src/blake3_avx2.c: fix AVX2 intrinsics and add const qualifiers
    loadu/storeu used undefined __m256i* casts and fixed array sizes,
    risking misalignment or truncation. Rotation masks recomputed on every
    call wasted cycles. Missing headers broke compilation. Implicit casts to
    uint8_t dropped bits.
    
    Add stdint.h/size_t.h. Assert __m256i size. Cast loadu/storeu to
    __m256i_u*. Static-ize rot16 mask, const-ize rot8/round_fn params. Use
    DEGREE macro. Explicit uint8_t casts for flags. Const-ify
    block_len_vec/block_flags_vec. Fix out pointer arithmetic.

--- src/blake3_avx2.c
+++ src/blake3_avx2.c
@@ -1,22 +1,26 @@
 #include "blake3_impl.h"
 
 #include <immintrin.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #define DEGREE 8
 
-INLINE __m256i loadu(const uint8_t src[32]) {
-    return _mm256_loadu_si256((const __m256i*)src);
+_Static_assert(sizeof(__m256i) == 32, "__m256i must be 32 bytes");
+
+INLINE __m256i loadu(const uint8_t* src) {
+    return _mm256_loadu_si256((const __m256i_u*)src);
 }
 
-INLINE void storeu(__m256i src, uint8_t dest[16]) {
-    _mm256_storeu_si256((__m256i*)dest, src);
+INLINE void storeu(__m256i src, uint8_t* dest) {
+    _mm256_storeu_si256((__m256i_u*)dest, src);
 }
 
 INLINE __m256i addv(__m256i a, __m256i b) {
     return _mm256_add_epi32(a, b);
 }
 
-// Note that clang-format doesn't like the name "xor" for some reason.
+// Note that clang-format doesn't like the name "xor" for some reason
 INLINE __m256i xorv(__m256i a, __m256i b) {
     return _mm256_xor_si256(a, b);
 }
@@ -26,7 +30,8 @@ INLINE __m256i set1(uint32_t x) {
 }
 
 INLINE __m256i rot16(__m256i x) {
-    return _mm256_shuffle_epi8(x, _mm256_set_epi8(13, 12, 15, 14, 9, 8, 11, 10, 5, 4, 7, 6, 1, 0, 3, 2, 13, 12, 15, 14, 9, 8, 11, 10, 5, 4, 7, 6, 1, 0, 3, 2));
+    static const __m256i mask = (const __m256i)_mm256_set_epi8(13, 12, 15, 14, 9, 8, 11, 10, 5, 4, 7, 6, 1, 0, 3, 2, 13, 12, 15, 14, 9, 8, 11, 10, 5, 4, 7, 6, 1, 0, 3, 2);
+    return _mm256_shuffle_epi8(x, mask);
 }
 
 INLINE __m256i rot12(__m256i x) {
@@ -34,14 +39,15 @@ INLINE __m256i rot12(__m256i x) {
 }
 
 INLINE __m256i rot8(__m256i x) {
-    return _mm256_shuffle_epi8(x, _mm256_set_epi8(12, 15, 14, 13, 8, 11, 10, 9, 4, 7, 6, 5, 0, 3, 2, 1, 12, 15, 14, 13, 8, 11, 10, 9, 4, 7, 6, 5, 0, 3, 2, 1));
+    const __m256i mask = _mm256_set_epi8(12, 15, 14, 13, 8, 11, 10, 9, 4, 7, 6, 5, 0, 3, 2, 1, 12, 15, 14, 13, 8, 11, 10, 9, 4, 7, 6, 5, 0, 3, 2, 1);
+    return _mm256_shuffle_epi8(x, mask);
 }
 
 INLINE __m256i rot7(__m256i x) {
     return _mm256_or_si256(_mm256_srli_epi32(x, 7), _mm256_slli_epi32(x, 32 - 7));
 }
 
-INLINE void round_fn(__m256i v[16], __m256i m[16], size_t r) {
+INLINE void round_fn(__m256i v[16], const __m256i m[16], size_t r) {
     v[0] = addv(v[0], m[(size_t)MSG_SCHEDULE[r][0]]);
     v[1] = addv(v[1], m[(size_t)MSG_SCHEDULE[r][2]]);
     v[2] = addv(v[2], m[(size_t)MSG_SCHEDULE[r][4]]);
@@ -158,8 +164,6 @@ INLINE void round_fn(__m256i v[16], __m256i m[16], size_t r) {
 }
 
 INLINE void transpose_vecs(__m256i vecs[DEGREE]) {
-    // Interleave 32-bit lanes. The low unpack is lanes 00/11/44/55, and the high
-    // is 22/33/66/77.
     __m256i ab_0145 = _mm256_unpacklo_epi32(vecs[0], vecs[1]);
     __m256i ab_2367 = _mm256_unpackhi_epi32(vecs[0], vecs[1]);
     __m256i cd_0145 = _mm256_unpacklo_epi32(vecs[2], vecs[3]);
@@ -169,8 +173,6 @@ INLINE void transpose_vecs(__m256i vecs[DEGREE]) {
     __m256i gh_0145 = _mm256_unpacklo_epi32(vecs[6], vecs[7]);
     __m256i gh_2367 = _mm256_unpackhi_epi32(vecs[6], vecs[7]);
 
-    // Interleave 64-bit lanes. The low unpack is lanes 00/22 and the high is
-    // 11/33.
     __m256i abcd_04 = _mm256_unpacklo_epi64(ab_0145, cd_0145);
     __m256i abcd_15 = _mm256_unpackhi_epi64(ab_0145, cd_0145);
     __m256i abcd_26 = _mm256_unpacklo_epi64(ab_2367, cd_2367);
@@ -180,7 +182,6 @@ INLINE void transpose_vecs(__m256i vecs[DEGREE]) {
     __m256i efgh_26 = _mm256_unpacklo_epi64(ef_2367, gh_2367);
     __m256i efgh_37 = _mm256_unpackhi_epi64(ef_2367, gh_2367);
 
-    // Interleave 128-bit lanes.
     vecs[0] = _mm256_permute2x128_si256(abcd_04, efgh_04, 0x20);
     vecs[1] = _mm256_permute2x128_si256(abcd_15, efgh_15, 0x20);
     vecs[2] = _mm256_permute2x128_si256(abcd_26, efgh_26, 0x20);
@@ -208,7 +209,7 @@ INLINE void transpose_msg_vecs(const uint8_t* const* inputs, size_t block_offset
     out[13] = loadu(&inputs[5][block_offset + 1 * sizeof(__m256i)]);
     out[14] = loadu(&inputs[6][block_offset + 1 * sizeof(__m256i)]);
     out[15] = loadu(&inputs[7][block_offset + 1 * sizeof(__m256i)]);
-    for (size_t i = 0; i < 8; ++i) {
+    for (size_t i = 0; i < DEGREE; ++i) {
         _mm_prefetch((const void*)&inputs[i][block_offset + 256], _MM_HINT_T0);
     }
     transpose_vecs(&out[0]);
@@ -231,16 +232,20 @@ blake3_hash8_avx2(const uint8_t* const* inputs, size_t blocks, const uint32_t ke
     __m256i h_vecs[8] = {
         set1(key[0]), set1(key[1]), set1(key[2]), set1(key[3]), set1(key[4]), set1(key[5]), set1(key[6]), set1(key[7]),
     };
+
     __m256i counter_low_vec, counter_high_vec;
     load_counters(counter, increment_counter, &counter_low_vec, &counter_high_vec);
-    uint8_t block_flags = flags | flags_start;
+
+    uint8_t block_flags = (uint8_t)(flags | flags_start);
 
     for (size_t block = 0; block < blocks; block++) {
         if (block + 1 == blocks) {
-            block_flags |= flags_end;
+            block_flags = (uint8_t)(block_flags | flags_end);
         }
-        __m256i block_len_vec = set1(BLAKE3_BLOCK_LEN);
-        __m256i block_flags_vec = set1(block_flags);
+
+        const __m256i block_len_vec = set1(BLAKE3_BLOCK_LEN);
+        const __m256i block_flags_vec = set1(block_flags);
+
         __m256i msg_vecs[16];
         transpose_msg_vecs(inputs, block * BLAKE3_BLOCK_LEN, msg_vecs);
 
@@ -248,6 +253,7 @@ blake3_hash8_avx2(const uint8_t* const* inputs, size_t blocks, const uint32_t ke
             h_vecs[0],   h_vecs[1],   h_vecs[2],   h_vecs[3],   h_vecs[4],       h_vecs[5],        h_vecs[6],     h_vecs[7],
             set1(IV[0]), set1(IV[1]), set1(IV[2]), set1(IV[3]), counter_low_vec, counter_high_vec, block_len_vec, block_flags_vec,
         };
+
         round_fn(v, msg_vecs, 0);
         round_fn(v, msg_vecs, 1);
         round_fn(v, msg_vecs, 2);
@@ -255,6 +261,7 @@ blake3_hash8_avx2(const uint8_t* const* inputs, size_t blocks, const uint32_t ke
         round_fn(v, msg_vecs, 4);
         round_fn(v, msg_vecs, 5);
         round_fn(v, msg_vecs, 6);
+
         h_vecs[0] = xorv(v[0], v[8]);
         h_vecs[1] = xorv(v[1], v[9]);
         h_vecs[2] = xorv(v[2], v[10]);
@@ -268,6 +275,7 @@ blake3_hash8_avx2(const uint8_t* const* inputs, size_t blocks, const uint32_t ke
     }
 
     transpose_vecs(h_vecs);
+
     storeu(h_vecs[0], &out[0 * sizeof(__m256i)]);
     storeu(h_vecs[1], &out[1 * sizeof(__m256i)]);
     storeu(h_vecs[2], &out[2 * sizeof(__m256i)]);
@@ -314,13 +322,16 @@ void blake3_hash_many_avx2(const uint8_t* const* inputs,
                            uint8_t* out) {
     while (num_inputs >= DEGREE) {
         blake3_hash8_avx2(inputs, blocks, key, counter, increment_counter, flags, flags_start, flags_end, out);
+
         if (increment_counter) {
             counter += DEGREE;
         }
+
         inputs += DEGREE;
         num_inputs -= DEGREE;
-        out = &out[DEGREE * BLAKE3_OUT_LEN];
+        out += DEGREE * BLAKE3_OUT_LEN;
     }
+
 #if !defined(BLAKE3_NO_SSE41)
     blake3_hash_many_sse41(inputs, num_inputs, blocks, key, counter, increment_counter, flags, flags_start, flags_end, out);
 #else
